6ccd2c052ac0b5ab2cb2eedffb7e07006818a691
Maberry00 <maberry30@gmail.com>
2024-01-12 09:45:30 -0600

    method1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace MethodsExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine(); // input
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine(); // input 
+
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine(); // input
+
+            Console.WriteLine("What band do you like listening to?");
+            var band = Console.ReadLine(); // input
+
+            Console.WriteLine($"The big guy named {name} is a very interesting person." +
+                $" He loves the color {color} and his favorite band is {band} that's a crazy coincidence." +
+                $" His favorite animal is a {animal} I guess because they sometimes live in pastors which are also green.");
+
         }
     }
 }
